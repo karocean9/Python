1d3b48acd09c8095dc32396b7a73c02167a008a7
Karthik Sagar <karthiksagar9999@gmail.com>
2019-02-26 00:18:20 -0600

    Update regular_exp_validation.py

--- regular_exp_validation.py
+++ regular_exp_validation.py
@@ -1,6 +1,6 @@
-import re
+import re /*Package to Import regular expression*/
 for i in range(int(input())) : 
-    if re.match(r'[789]\d{9}$',input()): 
+    if re.match(r'[789]\d{9}$',input()): /*Regular expression checking using match */
         print('YES') 
     else: 
         print('NO')
